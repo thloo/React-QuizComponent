db3237a81cabe5fd67d72a912ab6ee425879c21a
Tiffany Loo <tloo2@fvfyj08qjk7m.us.aexp.com>
2019-06-26 13:45:56 -0400

    sad

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <div className={'QuizQuestion'}>this.quiz_questions[0].instruction_text</div>
+                <div className='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div>
             </div>
         )
     }
