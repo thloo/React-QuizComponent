8f80f66f561f73c5d8485b75d9bb17c3db26cfee
Tiffany Loo <tloo2@fvfyj08qjk7m.us.aexp.com>
2019-06-28 14:21:28 -0400

    sad

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,13 +10,14 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
     render() {
-        const isQuizEnd = ({quiz_position-1}===quizData.quiz_questions.length);
+        const isQuizEnd = ({quiz_position-1} === quizData.quiz_questions.length);
         return (
             <div>
                 {isQuizEnd ? <QuizEnd/> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
             </div>
         )
+    }
 }
 
 export default Quiz
